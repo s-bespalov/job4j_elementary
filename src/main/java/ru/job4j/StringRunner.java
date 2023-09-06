cfb5547da8c65231c2d22831f25293ec79a46831
Sergey Bespalov <sergey.bespalov@protonmail.com>
2023-09-06 22:22:20 +0300

    9.3. Математические операторы и составные операторы присваивания [#504905]

---
+++ src/main/java/ru/job4j/StringRunner.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class StringRunner {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += " But I am a newbie. ";
+        idea += 2022;
+        System.out.println(idea);
+    }
+}
