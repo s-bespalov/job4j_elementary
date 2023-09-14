e745d20d0ac220e099daefd86dce9c969f36e0dc
Sergey Bespalov <sergey.bespalov@protonmail.com>
2023-09-15 00:51:00 +0300

    4.5. Отладка программы в IDEA [#159464 #444975]

--- src/main/java/ru/job4j/condition/ThreeMax.java
+++ src/main/java/ru/job4j/condition/ThreeMax.java
@@ -3,9 +3,9 @@ package ru.job4j.condition;
 public class ThreeMax {
     public static int max(int first, int second, int third) {
         int result = third;
-        if (first > second && first > third) {
+        if (first >= second && first >= third) {
             result = first;
-        } else if (second > third) {
+        } else if (second >= third) {
             result = second;
         }
         return result;
