1e911d15664828fb3308f99612e30fc78a444a60
Sergey Bespalov <sergey.bespalov@protonmail.com>
2023-09-16 15:35:48 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -10,5 +10,14 @@ public class ArrayDefinition {
 
         float[] prices = new float[40];
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Franklin Roosevelt";
+        names[1] = "George Washington";
+        names[2] = "Dwight Eisenhower";
+        names[3] = "Abraham Lincoln";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
