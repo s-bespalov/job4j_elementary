6958d0cb1572c7660737ffc3606d8e750a70e34b
Sergey Bespalov <sergey.bespalov@protonmail.com>
2023-09-17 01:32:38 +0300

    6.5.2. Поиск минимума в диапазоне. [#179804]

---
+++ src/main/java/ru/job4j/array/MinDiapason.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class MinDiapason {
+    public static int findMin(int[] array, int start, int finish) {
+        int min = -1;
+        if (start >= 0 && start <= finish && finish < array.length) {
+            min = array[start];
+            for (int index = start + 1; index <= finish; index++) {
+                min = array[index] < min ? array[index] : min;
+            }
+        }
+        return min;
+    }
+}
