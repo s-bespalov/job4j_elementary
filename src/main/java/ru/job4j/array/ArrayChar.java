80dd20fd8f308137b821260b62dded1c6243ac33
Sergey Bespalov <sergey.bespalov@protonmail.com>
2023-09-17 00:50:53 +0300

    6.4.0. Слово начинается с ... [#41585]

---
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        if (pref.length > word.length) {
+            result = false;
+        } else {
+            for (int index = 0; index < pref.length; index++) {
+                if (word[index] != pref[index]) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
