8e116867efa3fd82fbf5e8b3f4c452a56f8151ab
Sergey Bespalov <sergey.bespalov@protonmail.com>
2023-09-17 03:43:37 +0300

    6.7.1. Моно строка в матрице. [#214126]

---
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MatrixCheckTest {
+    @Test
+    public void whenHasMonoHorizontal() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '},
+        };
+        int row = 1;
+        boolean result = MatrixCheck.monoHorizontal(input, row);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenDoesNotHaveMonoHorizontalInRow4() {
+        char[][] input = {
+                {' ', ' ', ' ', 'X', 'X'},
+                {'X', 'X', 'X', ' ', 'X'},
+                {' ', ' ', ' ', 'X', ' '},
+                {'X', 'X', ' ', 'X', ' '},
+                {'X', 'X', 'X', 'X', ' '},
+        };
+        int row = 4;
+        boolean result = MatrixCheck.monoHorizontal(input, row);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void whenDoesNotHaveMonoHorizontalInRow1() {
+        char[][] input = {
+                {' ', ' ', ' ', 'X', 'X'},
+                {'X', 'X', 'X', ' ', 'X'},
+                {' ', ' ', ' ', 'X', ' '},
+                {'X', 'X', ' ', 'X', ' '},
+                {'X', 'X', 'X', 'X', ' '},
+        };
+        int row = 4;
+        boolean result = MatrixCheck.monoHorizontal(input, row);
+        assertThat(result).isFalse();
+    }
+}
